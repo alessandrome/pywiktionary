5209e02143fd203716f05b5586b71a0a8e419d6f
Alessandro Mesti <mesti.alessandro.github@gmail.com>
2019-08-07 00:34:53 +0200

    Test: Add test for BasicParser class

--- tests/unittest/parser/test_basic_parser.py
+++ tests/unittest/parser/test_basic_parser.py
@@ -1,9 +1,21 @@
 import unittest
+from pywiktionary.parsers import basic_parser
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+def get_pizza_html_extract():
+    with open('../../file/html-responses/pizza-it.html', 'r') as pizza_html_file:
+        pizza_html = pizza_html_file.read()
+    return pizza_html
+
+
+class BasicParseTestCase(unittest.TestCase):
+    def test_init(self):
+        parser = basic_parser.BasicParser(get_pizza_html_extract())
+        self.assertEqual(get_pizza_html_extract(), parser.html)
+
+    def test_parse_method(self):
+        parser = basic_parser.BasicParser({})
+        self.assertRaises(NotImplementedError, parser.parse)
 
 
 if __name__ == '__main__':
